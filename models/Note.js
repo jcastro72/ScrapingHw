86cfe57006d709ab8c9a97b33ecbf2dfcf915e5d
jcastro15 <castrojake99@gmail.com>
2018-09-20 21:18:50 -0400

    back to the old

--- models/Note.js
+++ models/Note.js
@@ -5,17 +5,14 @@ var Schema = mongoose.Schema;
 
 // Create the Note schema
 var NoteSchema = new Schema({
-  // Just a string
-  article_id: {
-    type: Schema.Types.ObjectId
-  },
-  body: {
-    type: String
-  }
+    body: {
+        type: String
+    },
+    article: {
+        type: Schema.Types.ObjectId,
+        ref: "Article"
+    }
 });
-
-// Remember, Mongoose will automatically save the ObjectIds of the notes
-// These ids are referred to in the Article model
 
 // Create the Note model with the NoteSchema
 var Note = mongoose.model("Note", NoteSchema);
